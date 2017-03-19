96c7dc1de693860c495f392e2ed66e491002f683
ninjaPixel <matthew@ninjaPixel.eu>
2017-03-19 12:23:59 +0000

    Simple navigator stack

--- app/main.js
+++ app/main.js
@@ -1,24 +1,125 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button, ScrollView} from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
+
+class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    static navigationOptions = {
+        title: 'React Navigator Demo',
+    };
+    
+    
+    renderCountryButtons() {
+        const countriesAndCities = [
+            {
+                country: 'America',
+                cities: [{city: 'New York'}, {city: 'San Francisco'}, {city: 'Louisville'}, {city: 'Seattle'}]
+            },
+            {
+                country: 'England',
+                cities: [{city: 'London'}, {city: 'Cambridge'}, {city: 'Oxford'}, {city: 'Manchester'}]
+            },
+        ];
+        
+        return countriesAndCities.map((d, i)=> {
+            const {navigate} = this.props.navigation;
+            return (
+              <View style={styles.button} key={i}>
+                  <Button onPress={() => navigate('Country', {country: d.country, cities: d.cities})}
+                          title={d.country}
+                  /></View>
+            );
+        });
+    }
+    
+    render() {
+        return (
+          <ScrollView style={styles.container}>
+              <Text style={styles.text}>This simple app demos the features of react-navigator.</Text>
+              <Text style={styles.text}>Choose a country to visit:</Text>
+              {this.renderCountryButtons()}
+          </ScrollView>
+        );
+    }
 }
 
+
+class CountryScreen extends React.Component {
+    static navigationOptions = {
+        title: ({state}) => state.params.country
+    };
+    
+    renderCities() {
+        const {navigate} = this.props.navigation;
+        return this.props.navigation.state.params.cities.map((d, i)=> {
+            return (
+              <View style={styles.button} key={i}>
+                  <Button onPress={() => navigate('City', {city: d.city})}
+                          title={d.city}
+                  /></View>
+            );
+        });
+    }
+    
+    render() {
+        return (
+          <View>
+              <Text style={styles.text}>Items (routes) are added to the Navigation stack</Text>
+              {this.renderCities()}
+          </View>
+        );
+    }
+}
+
+class CityScreen extends React.Component {
+    static navigationOptions = {
+        title: ({state}) => {
+            return state.params.city;
+        },
+    };
+    
+    render() {
+        return (
+          <View>
+              <Text style={styles.text}>You can use tabs, too. Note that these are contained within thier parent route.</Text>
+              <Text style={styles.text}>{lorem}</Text>
+          </View>
+        );
+    }
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    statusBarUnderlay: {
+        height: 24,
+        backgroundColor: 'rgba(0,0,0,0.2)',
+    },
+    text: {
+        padding: 10,
+    },
+    button: {
+        padding: 10,
+    }
 });
 
-Expo.registerRootComponent(App);
+const lorem = "Marfa bushwick distillery venmo readymade, seitan taxidermy single-origin coffee. Fashion axe iPhone pug, edison bulb pinterest bicycle rights intelligentsia chartreuse cronut try-hard subway tile blog typewriter. Microdosing pitchfork kogi forage, vape fixie green juice austin fam fap cray poutine bespoke art party. Kale chips hella meggings neutra put a bird on it la croix, kombucha try-hard stumptown disrupt pour-over. Fam beard migas, pinterest poke woke intelligentsia franzen 90's raw denim af vaporware vinyl. Woke affogato intelligentsia gochujang, schlitz tumblr authentic artisan echo park kickstarter pour-over food truck retro hashtag. Hammock thundercats four loko messenger bag unicorn keytar, dreamcatcher truffaut poutine lumbersexual flannel heirloom photo booth biodiesel gochujang.";
+
+
+const SimpleApp = StackNavigator({
+    Home: {screen: HomeScreen},
+    Country: {screen: CountryScreen},
+    City: {screen: CityScreen},
+});
+
+
+Expo.registerRootComponent(SimpleApp);
+
