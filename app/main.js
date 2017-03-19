5f50babdc870f2542e273795b1f6a51469f37504
ninjaPixel <matthew@ninjaPixel.eu>
2017-03-19 16:57:22 +0000

    Add markers to the map

--- app/main.js
+++ app/main.js
@@ -34,9 +34,10 @@ class HomeScreen extends React.Component {
                 cities: [{city: 'Paris'}, {city: 'Nantes'}, {city: 'Cannes'}, {city: 'Bordeaux'}]
             },
         ];
+        const {navigate} = this.props.navigation;
+        
         
         return countriesAndCities.map((d, i)=> {
-            const {navigate} = this.props.navigation;
             return (
               <View style={styles.button} key={i}>
                   <Button onPress={() => navigate('Country', {country: d.country, cities: d.cities})}
@@ -118,6 +119,42 @@ class CityInfoScreen extends React.Component {
 }
 
 class CityMapScreen extends React.Component {
+    
+    renderMarkers() {
+        const markers = [{
+            latlng: {
+                latitude: 37.78825,
+                longitude: -122.4324
+            },
+            title: 'Title',
+            description: 'Some interesting description'
+        },
+        {
+            latlng: {
+                latitude: 37.78925,
+                longitude: -122.4334
+            },
+            title: 'Title',
+            description: 'Some interesting description'
+        },
+        {
+            latlng: {
+                latitude: 37.78685,
+                longitude: -122.4421
+            },
+            title: 'Title',
+            description: 'Some interesting description'
+        }];
+        return (    markers.map((marker,i) =>
+          <Expo.MapView.Marker
+            key={i}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ));
+    }
+    
     render() {
         return (
           <Expo.MapView
@@ -128,7 +165,9 @@ class CityMapScreen extends React.Component {
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}
-          />
+          >
+              {this.renderMarkers()}
+          </Expo.MapView>
         
         );
     }
